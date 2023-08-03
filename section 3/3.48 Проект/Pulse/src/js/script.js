b786f92b875e9b540468b8830fdc462cc6c5f44a
Yury Klintsou <yuriklintsou@gmail.com>
2023-08-04 00:26:16 +0300

    3.53 pulse project slick-slider

---
+++ section 3/3.48 Проект/Pulse/src/js/script.js
@@ -0,0 +1,23 @@
+// $ - библиотека jQuery
+// slick - https://kenwheeler.github.io/slick/
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        // infinite: true,
+        // slidesToShow: 1,
+        // slidesToScroll: 1
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/slider/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/slider/right.png"></button>',
+        responsive: [
+            {
+                //для мобил чтобы появились точки вместо стрелочек
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+      });
+  });
