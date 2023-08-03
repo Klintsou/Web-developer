b6dc9a1026c267a9b4d9b98ca31edbcd49745263
Yury Klintsou <yuriklintsou@gmail.com>
2023-08-04 00:33:03 +0300

    3.53 pulse project slick-slider (fixing img/scripts paths)

--- section 3/3.48 Проект/Pulse/src/js/script.js
+++ section 3/3.48 Проект/Pulse/src/js/script.js
@@ -7,8 +7,8 @@ $(document).ready(function(){
         // slidesToScroll: 1
         speed: 1200,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../img/slider/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/slider/right.png"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/slider/right.png"></button>',
         responsive: [
             {
                 //для мобил чтобы появились точки вместо стрелочек
